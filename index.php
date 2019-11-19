12a9b2c8aa1b626949dc73af0fe45deb33acbc4c
MauVgs <43446262+MauVgs@users.noreply.github.com>
2019-11-19 11:44:58 -0600

    Cambio de Interfaz gráfica

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php 
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,18 +10,18 @@
     <title>Document</title>
     <link rel="stylesheet" href="/css/styles.css" type="text/css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+    <link rel="stylesheet" href="https://use.typekit.net/vcy1rfg.css">
 </head>
 <body>
-
     <header>
         <div class="navBarHome">
             <div>
                 <a href="/index.php"><img src="/public/img/logo.png" alt="Techies Blog" class="logo"></a>
             </div>
-            <div class="dropdown is-hoverable">
+            <div class="dropdown is-hoverable ml">
                 <div class="dropdown-trigger">
                     <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
-                    <span>Categorías</span>
+                    <span class="has-text-info">Categorías</span>
                     <span class="icon is-small">
                         <i class="fas fa-angle-down" aria-hidden="true"></i>
                     </span>
@@ -27,10 +30,10 @@
                 <div class="dropdown-menu" id="dropdown-menu4" role="menu">
                     <div class="dropdown-content">
                     <div class="dropdown-item">
-                        <a href=""><span>Ejemplo 1</span></a>
+                        <a href="/public/views/detalles.php"><span class="has-text-info">Categoria 1</span></a>
                     </div>
                     <div class="dropdown-item">
-                        <a href=""><span>Ejemplo 1</span></a>
+                        <a href="/public/views/detalles.php"><span class="has-text-info">Categoria 2</span></a>
                     </div>
                     </div>
                 </div>
@@ -44,10 +47,10 @@
                     <div class="icon">
                         <img src="" alt="">
                     </div>
-                    <div class="desc">
+                    <div class="titleArticle">
                         <h6>Peace of Mind</h6>
                     </div>
-                    <div class="p1">
+                    <div class="infoArticle">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                             Cras ultrices accumsan ornare. Phasellus tristique</p>
                     </div>
@@ -56,62 +59,16 @@
                     <div class="icon">
                         <img src="" alt="">
                     </div>
-                    <div class="desc">
+                    <div class="titleArticle">
                         <h6>Peace of Mind</h6>
                     </div>
-                    <div class="p1">
+                    <div class="infoArticle">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                             Cras ultrices accumsan ornare. Phasellus tristique</p>
                     </div>
                 </article>
-                <article class="articulo">
-                    <div class="icon">
-                        <img src="" alt="">
-                    </div>
-                    <div class="desc">
-                        <h6>Peace of Mind</h6>
-                    </div>
-                    <div class="p1">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                            Cras ultrices accumsan ornare. Phasellus tristique</p>
-                    </div>
-                </article>
-                <article class="articulo">
-                    <div class="icon">
-                        <img src="" alt="">
-                    </div>
-                    <div class="desc">
-                        <h6>Peace of Mind</h6>
-                    </div>
-                    <div class="p1">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                            Cras ultrices accumsan ornare. Phasellus tristique</p>
-                    </div>
-                </article>
-                <article class="articulo">
-                    <div class="icon">
-                        <img src="" alt="">
-                    </div>
-                    <div class="desc">
-                        <h6>Peace of Mind</h6>
-                    </div>
-                    <div class="p1">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                            Cras ultrices accumsan ornare. Phasellus tristique</p>
-                    </div>
-                </article>
-                <article class="articulo">
-                    <div class="icon">
-                        <img src="" alt="">
-                    </div>
-                    <div class="desc">
-                        <h6>Peace of Mind</h6>
-                    </div>
-                    <div class="p1">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                            Cras ultrices accumsan ornare. Phasellus tristique</p>
-                    </div>
-                </article>
+
+
         </section>
     </main>
 </body>
