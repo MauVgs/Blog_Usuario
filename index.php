483a615f96bae2694ea51b31348bd7604c78ef90
MauVgs <43446262+MauVgs@users.noreply.github.com>
2019-11-18 18:26:43 -0600

    Primer commit (Interfaz de Usuario)

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/styles.css" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+</head>
+<body>
+    <header>
+        <div class="navBarHome">
+            <div>
+                <a href="/index.php"><img src="/public/img/logo.png" alt="Techies Blog" class="logo"></a>
+            </div>
+        </div>
+    </header>
+    <main class="main">
+        <div class="container has-text-centered">
+            <div class="title">
+                <h1>¡Tu tienes una duda, nosotros la respuesta!</h1>
+            </div>
+            <div class="imgHome">
+                <img src="/public/img/home.png" alt="">
+            </div>
+            <div class="divBtn">
+                <a href="/public/views/category.php"><button class="btnBlog">Ingresar</button></a>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
